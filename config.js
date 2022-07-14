9dc8c53ff053b81ec862d41747cef82f8f60c95f
thanhthuong3435 <85285327+thanhthuong3435@users.noreply.github.com>
2022-07-14 21:55:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
     introTitle: 'Cậu à!',
     introDesc: `Vào một ngày đẹp trời
-    Tớ nhận ra tớ thật sự xinh đẹp
+    Tớ nhận ra cậu thật sự xinh đẹp
     Trả lời tớ câu hỏi này`,
     btnIntro: 'Oki',
-    title: 'Diễm rất xinh đẹp ',
+    title: 'Cậu rất xinh đẹp ',
     desc: 'Đúng không?',
     btnYes: 'Đúng vậy, đẹp xuất sắc',
     btnNo: 'Không:3',
     question:'Tớ biết mà cậu muốn nói điều gì với tớ không?',
-    btnReply: 'Gửi cho Diễm xinh gái',
-    reply: 'Ok, bạn diễm xinh đẹp',
+    btnReply: 'Gửi cho ... xinh gái',
+    reply: 'Ok, xinh đẹp',
     mess: ' Tớ biết mà 🥰. Thank kiuuuuuu😘😘',
-    messDesc: 'Từ giờ hãy gọi tớ là Diễm xinh đẹp nhé',
+    messDesc: 'Từ giờ hãy gọi tớ là  xinh đẹp nhé',
     btnAccept: 'Okiiiii',
     messLink: 'https://www.facebook.com/diemtran.2806' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
